055d5660fc8a74bb9792ace876c00feb51dff7c8
zodiac <jiristajer9@gmail.com>
2024-06-20 20:48:14 +0200

    Malé změny

--- viewer/models.py
+++ viewer/models.py
@@ -12,7 +12,7 @@ class Event(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
     type = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-   #location = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.title
