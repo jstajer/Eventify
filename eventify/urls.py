e10d144bcd32ae53b64beea350ba05d43531c9cc
zodiac <jiristajer9@gmail.com>
2024-06-20 20:24:07 +0200

    Malé změny

--- eventify/urls.py
+++ eventify/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
 from viewer.views import *
+from django.contrib import admin
 
 urlpatterns = [
+
+    path('admin/', admin.site.urls),
     path('',home, name='home'),
     path('event/<int:event_id>/', event_detail, name='event_detail'),
     path('event/<int:event_id>/register/', register_for_event, name='register_for_event'),
