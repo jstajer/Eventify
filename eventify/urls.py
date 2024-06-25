cb32d82e33fbf1f7f1421675884003aaea07ba08
zodiac <jiristajer9@gmail.com>
2024-06-25 17:58:22 +0200

    Events

--- eventify/urls.py
+++ eventify/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('',home, name='home'),
+    path('', home, name='home'),
     path('event/<int:event_id>/', event_detail, name='event_detail'),
     path('event/<int:event_id>/register/', register_for_event, name='register_for_event'),
     path('create/', create_event, name='create_event'),
