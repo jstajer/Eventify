f3a0b567d9ec2c77c7e41173ac65566561f87d61
zodiac <jiristajer9@gmail.com>
2024-06-26 17:44:12 +0200

    změny na gitu.

--- viewer/forms.py
+++ viewer/forms.py
@@ -5,5 +5,5 @@ from .models import Event
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'start_date', 'end_date', 'description', 'type', 'price', 'location']
+        fields = ['title', 'start_date', 'end_date', 'description', 'type', 'price', 'location', 'district']
 
--- viewer/models.py
+++ viewer/models.py
@@ -13,6 +13,7 @@ class Event(models.Model):
     type = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.URLField(max_length=200, null=True, blank=True)
+    district = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.title
