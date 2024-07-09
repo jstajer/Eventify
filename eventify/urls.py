d2263c5a7c1f4ba4add3433c1d26bc5cc74132e2
Michal Maják <michalmajak638@gmail.com>
2024-07-09 19:48:58 +0200

    API-Upgrade

--- eventify/urls.py
+++ eventify/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+
+import rest_framework
+
+from api.views import *
+import api
 from viewer import views
 from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,4 +22,7 @@ urlpatterns = [
     path('search/', views.search_events, name='search_events'),
     path('signup/', views.signup, name='signup'),
     path('events/<str:filter_type>/', views.filtered_events, name='filtered_events'),
+
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/eventify/', api.views.Events.as_view()),
 ]
