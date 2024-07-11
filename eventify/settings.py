6ddd59ac455661f3ca7f5666e6120b9e3ab59fc0
Michal Maják <michalmajak638@gmail.com>
2024-07-11 21:11:58 +0200

    Upgrade Search(Jiří)

--- eventify/settings.py
+++ eventify/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'eventify.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', # Databáze, vždy vkládat do gitignore
     }
 }
 
