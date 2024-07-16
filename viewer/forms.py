abf84be12f591bf1dcbec48a892edd2af3a87aac
byll <martin.byll.havranek@gmail.com>
2024-07-16 21:46:58 +0200

    Úprava výběru datumu

--- viewer/forms.py
+++ viewer/forms.py
@@ -8,5 +8,7 @@ class EventForm(forms.ModelForm):
         model = Event
         fields = '__all__'
         widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'datetime-local'}),
+            'end_date': forms.DateInput(attrs={'type': 'datetime-local'}),
             'district': forms.Select(choices=REGION_CHOICES),
         }
